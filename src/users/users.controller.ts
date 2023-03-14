c74253512bee8939681fec8ee6a7ffff2f6ada79
arm-inqnatevosyan <inqnatevosyan06@mail.ru>
2023-03-14 22:48:28 +0400

    new

---
+++ src/users/users.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Put,
+  Delete,
+  Param,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity/user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private service: UsersService) {}
+
+  @Get()
+  gets(@Param() params) {
+    return this.service.FullUsers(params.id);
+  }
+
+  @Get(':id')
+  get(@Param() params) {
+    return this.service.getUsers(params.id);
+  }
+
+  @Post()
+  create(@Body() user: User) {
+    return this.service.createUser(user);
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: User) {
+    return this.service.createUser(createUserDto);
+  }
+  @Put(':id')
+  update(@Param() params, @Body() user: User) {
+    return this.service.updateUser(params.id, user);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param() params) {
+    return this.service.deleteUser(params.id);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  async getUsers(user: User): Promise<User[]> {
+    return await this.usersRepository.find();
+  }
+  async FullUsers(user: User): Promise<User[]> {
+    return await this.usersRepository.find();
+  }
+
+  async createUser(userDetails: User) {
+    const newUser = this.usersRepository.create({
+      ...userDetails,
+    });
+    return await this.usersRepository.save(newUser);
+  }
+  async updateUser(id: number, UserDetails: User) {
+    return await this.usersRepository.update({ id }, { ...UserDetails });
+  }
+
+  async deleteUser(user: User) {
+    this.usersRepository.delete(user);
+  }
+}
